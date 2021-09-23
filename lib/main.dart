7ea41fd30d991f3f5a4267457607c72dc4ec83ee
jaev-977 <john-977@hotmail.com>
2021-09-23 18:47:24 -0500

    diseño principal

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,15 @@ import 'package:casanareapp/pages/home.pages.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApplication());
+  runApp(const MyApplication());
 }
 
 class MyApplication extends StatelessWidget {
-  MyApplication({Key? key}) : super(key: key);
-
-  final ColorScheme colorScheme = ColorScheme(
-    primary: Color.fromARGB(33, 37, 41, 1), // <---- I set white color here
-    primaryVariant: Color(0xFF117378),
-    secondary: Color(0xFFEFF3F3),
-    secondaryVariant: Color(0xFFFAFBFB),
-    background: Color(0xFF636363),
-    surface: Color(0xFF808080),
-    onBackground: Colors.white,
-    error: Colors.redAccent,
-    onError: Colors.redAccent,
-    onPrimary: Colors.redAccent,
-    onSecondary: Color(0xFF322942),
-    onSurface: Color(0xFF241E30),
-    brightness: Brightness.light,
-  );
+  const MyApplication({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       //Pagina principal CasanareApp
 
       home: HomePage(),
--- lib/pages/home.pages.dart
+++ lib/pages/home.pages.dart
@@ -1,79 +1,211 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  bool _pinned = true;
-  bool _snap = false;
-  bool _floating = false;
-  Object? values;
-  List listMunicipio = ["Yopal", "aguazul"];
-  List tipoLugar = ["Transporte", "compras"];
+  final bool _pinned = true;
+  final bool _snap = false;
+  final bool _floating = false;
+  Object? values, values1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          pinned: _pinned,
-          snap: _snap,
-          floating: _floating,
-          expandedHeight: 100.0,
-          flexibleSpace: new Image.network(
-              'https://www.casanare.app/img/core-img/cd-logo_white.png'),
-          actions: <Widget>[
-            IconButton(
-              icon: new Icon(Icons.menu),
-              alignment: Alignment.bottomLeft,
-              onPressed: () => print('hi on icon action'),
-            ),
-          ],
-        ),
-        const SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 20,
-              child: Center(
-                child: Text('Todo lo que buscas está en un solo lugar'),
+        body: Container(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: _pinned,
+            snap: _snap,
+            floating: _floating,
+            expandedHeight: 100.0,
+            backgroundColor: const Color.fromRGBO(33, 37, 41, 10),
+            flexibleSpace: Image.network(
+                'https://www.casanare.app/img/core-img/cd-logo_white.png'),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.menu),
+                alignment: Alignment.bottomLeft,
+                // ignore: avoid_print
+                onPressed: () => print('hi on icon action'),
+              ),
+            ],
+          ),
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(100, 10, 100, 80),
+              child: SizedBox(
+                height: 80,
+                child: Center(
+                  child: Text(
+                    'Todo lo que buscas está en un solo lugar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 27,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
+          SliverFixedExtentList(
+            itemExtent: 55,
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
                 child: Container(
-                  child: DropdownButton(
-                    value: values,
-                    onChanged: (newValue) {
-                      setState(() {
-                        values = newValue;
-                      });
-                    },
-                    items: <String>["Yopal", "aguazul"]
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
+                  child: const Text(
+                    'Directorio',
+                    style: TextStyle(
+                      color: Colors.white,
+                      backgroundColor: Color.fromRGBO(67, 25, 161, 0.90),
+                    ),
                   ),
+                  alignment: Alignment.bottomLeft,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(15)),
                 ),
-              );
-            },
-            childCount: 2,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+                child: Container(
+                    child: const Text(
+                      '¿Que estas Buscando?',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(67, 25, 161, 0.90),
+                        border: Border.all(color: Colors.blueAccent),
+                        borderRadius: BorderRadius.circular(8))),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+                child: Container(
+                    child: DropdownButton(
+                      value: values,
+                      onChanged: (newValue) {
+                        setState(() {
+                          values = newValue;
+                        });
+                      },
+                      iconSize: 36,
+                      icon: const Icon(Icons.arrow_drop_down,
+                          color: Colors.black),
+                      isExpanded: true,
+                      items: <String>["Yopal", "aguazul"]
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value,
+                              style: const TextStyle(
+                                color: Colors.white,
+                              )),
+                        );
+                      }).toList(),
+                      dropdownColor: Colors.black54,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(67, 25, 161, 0.90),
+                      border: Border.all(color: Colors.blueAccent, width: 2),
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+                child: Container(
+                    child: DropdownButton(
+                      value: values1,
+                      onChanged: (newValue1) {
+                        setState(() {
+                          values1 = newValue1;
+                        });
+                      },
+                      iconSize: 36,
+                      icon: const Icon(Icons.arrow_drop_down,
+                          color: Colors.black),
+                      isExpanded: true,
+                      items: <String>["Transporte", "compras"]
+                          .map<DropdownMenuItem<String>>((String values1) {
+                        return DropdownMenuItem<String>(
+                          value: values1,
+                          child: Text(values1,
+                              style: const TextStyle(
+                                color: Colors.white,
+                              )),
+                        );
+                      }).toList(),
+                      dropdownColor: Colors.black54,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(67, 25, 161, 0.90),
+                      border: Border.all(color: Colors.blueAccent, width: 2),
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+                child: Container(
+                    child: Center(
+                      child: Column(
+                        children: const [
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Palabras Clave de la Busqueda",
+                              fillColor: Colors.white,
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(67, 25, 161, 0.90),
+                        border: Border.all(color: Colors.blueAccent),
+                        borderRadius: BorderRadius.circular(8))),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+                child: Container(
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromRGBO(118, 67, 234, 1)),
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(10)),
+                      textStyle: MaterialStateProperty.all(
+                          const TextStyle(fontSize: 20)),
+                    ),
+                    icon: const Icon(Icons.search, size: 18),
+                    label: const Text('BUSCAR'),
+                  ),
+                  alignment: Alignment.topRight,
+                ),
+              ),
+            ]),
           ),
-        ),
-      ],
+        ],
+      ),
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage("/casanare.jpg"),
+        scale: 1000, // <-- BACKGROUND IMAGE
+        fit: BoxFit.cover,
+      )),
     ));
   }
 }
